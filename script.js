98f5726ab8a42ef718ab876edceeb82aa4782ce3
Dan Weikart <danmweikart@gmail.com>
2020-03-18 23:40:09 -0700

    finalized password generator using .push and .join

--- script.js
+++ script.js
@@ -2,83 +2,74 @@
  * WRITE YOUR CODE HERE
  */
 
-//  Define arrays
+//  Define arrays by character type
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var capital = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "/", ":", ";", "<", ">", "?", "[", "]", "_", "`", "|", "{", "}", "~"];
 
-// Define variables used to create random character from each array.
-
-// define variable to store random item from array lowercase
-// Math.random() generates random decimal
-// * lowercase.length multiplies that decimal by length of array
-// Match.floor() rounds that value down to whole number
-// return lowercase[] uses that whole number as the index of the array and returns that value
-var lowRandom = lowercase[Math.floor(Math.random() * lowercase.length)];
-// generates random capital letter.
-var capRandom = capital[Math.floor(Math.random() * capital.length)];
-// generates random number.
-var numRandom = number[Math.floor(Math.random() * number.length)];
-// generates random special character.
-var specRandom = special[Math.floor(Math.random() * special.length)];
-
-//random value from arrays are checked in console 
-// console.log(lowRandom, capRandom, numRandom, specRandom);
-
-//define password length variable outside function so it still exists.
-// ***********does not seem to retain user input value outside of function...
+//define password length variable outside function so it still exists
 var passLength = "";
+// define passType array to store user requirements on character types
+var passType = [];
 
 // adds event listener for when generate password button is clicked
 document.getElementById("generate").addEventListener("click", userInput)
 
-// function userInput runs when Generate Password button is clicked.
+// function userInput runs when Generate Password button is clicked
 function userInput(){
  // define user confirms for character types
-  // var lcase = confirm("Do you want lowercase letters in your password?");
-  // var caps = confirm("Do you want capitcal letters in your password?");
-  // var nums = confirm("Do you want numbers in your password?");
-  // var spec = confirm("Do you want special characters in your password?");
+  var lcase = confirm("Do you want lowercase letters in your password?");
+  var caps = confirm("Do you want capitcal letters in your password?");
+  var nums = confirm("Do you want numbers in your password?");
+  var spec = confirm("Do you want special characters in your password?");
 
-// //Have user define password length
-  var passLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.");
-// check to make sure user input is a number between 8 and 128.
-  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
-    alert("You must enter a number between 8 and 128.");
-  // will keep prompting until number between 8 and 128 is chosen.
-    var passLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.");
-}
- // once conditions are met will inform user of choice.
-  alert("You have chosen a length of " + passLength);
-  
-}
-
-//  **********Somehow need to make user input of var passLength exist outside of function...***
-
-
-// *****Need if statement to check which character arrays to use.
-// *****Do I add arrays? create object?
-
-// if (lcase == true && caps == true && nums == true && spec == true) {
-  passType = [...lowercase, ...capital, ...number, ...special];
-// }  
-  // console.log(passType.length);
-  // console.log(passType);
-
-var passkey = "";
-
-function generatePassword() {
-  for (var i = 0; i <= passLength.length; i++) {
-  var passChar = passType[Math.floor(Math.random() * passType.length)];
-
-    // ****not sure how to store string of all iterations****
-
+// add lowercase to passType if user wants to inlude lowercase
+  if (lcase) {
+   passType = passType.concat(lowercase);
+  }
+// add capital to passType if user wants to inlude caps
+  if (caps) {
+    passType = passType.concat(capital);
+  }
+// add nubmers to passType if user wants to inlude nums
+  if (nums) {
+    passType = passType.concat(number);
+  }
+// add special to passType if user wants to inlude special characters
+  if (spec) {
+    passType = passType.concat(special);
   }
 
-}
-// generatePassword();
+ //Have user define password length
+  passLength = prompt("How many characters do you want your password to be? Choose a number between 8 and 128.");
+// check to make sure user input is a number between 8 and 128
+  while (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+// will keep prompting until number between 8 and 128 is chosen
+  passLength = prompt("Please enter a number between 8 and 128.");
+  
+  }
+ // once conditions are met will inform user of choice
+  alert("You have chosen a length of " + passLength);
 
+}
+// define passChar array to store randomly generated characters
+var passChar = [];
+
+function generatePassword() {
+
+   for (var i = 0; i <= passLength; i++) {
+    
+    // Math.random() generates random decimal
+    // * passType.length multiplies that decimal by length of array
+    // Match.floor() rounds that value down to whole number
+    // passChar.push stores the random characters into the passChar array
+    passChar.push(passType[Math.floor(Math.random() * passType.length)]);
+
+  }
+  // returns the array values as a string
+  return passChar.join("");
+}
 //////////////////////////////////////////////////////////////
 // DO NOT TOUCH THE CODE BELOW
 //////////////////////////////////////////////////////////////
